3842bf7876b5284b81b6804ac6120e653430bf61
GoncharenkoVitaliy <Vitalesss.87@gmail.com>
2024-07-07 17:31:56 +0300

    coment-iframe

--- iframe.js
+++ iframe.js
@@ -25,26 +25,26 @@
 
 // pipedriveUI.modal.openByName('NEW Create a job three');
 
-function saveData() {
-	const link = 'https://goncharenkovitaliy.github.io/iframe-pipedrive/?code=13525530.21566062.3414aafa12ac87d2317a1241a393e0d952c21a3e';
-	// console.log('objData:', objData);
+// function saveData() {
+// 	const link = 'https://goncharenkovitaliy.github.io/iframe-pipedrive/?code=13525530.21566062.3414aafa12ac87d2317a1241a393e0d952c21a3e';
+// 	// console.log('objData:', objData);
 
-	fetch(link, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			title: 'New deal',
-			// value: objData
-		})
-	})
-	.then(response => {
-		if (!response.ok) {
-			throw new Error('The network response was incorrect');
-		}
-		console.log('response', response);
-		return response.json();
-	})
-	.catch(error => console.error('Fetch error:', error));
-}
+// 	fetch(link, {
+// 		method: 'POST',
+// 		headers: {
+// 			'Content-Type': 'application/json'
+// 		},
+// 		body: JSON.stringify({
+// 			title: 'New deal',
+// 			// value: objData
+// 		})
+// 	})
+// 	.then(response => {
+// 		if (!response.ok) {
+// 			throw new Error('The network response was incorrect');
+// 		}
+// 		console.log('response', response);
+// 		return response.json();
+// 	})
+// 	.catch(error => console.error('Fetch error:', error));
+// }
